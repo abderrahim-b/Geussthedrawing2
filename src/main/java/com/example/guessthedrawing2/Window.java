bcd50c779946a7acd6274e11561b7a39e1802aef
abderrahim-b <abderrahim.boukhelfa@etu.univ-batna2.dz>
2025-05-01 10:47:03 +0100

    rahim add a thing

---
+++ src/main/java/com/example/guessthedrawing2/Window.java
@@ -0,0 +1,19 @@
+package com.example.guessthedrawing2;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Window extends Application {
+
+    public void start(Stage stage) throws IOException {
+        Group root = new Group();
+        Scene scene = new Scene( root);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
